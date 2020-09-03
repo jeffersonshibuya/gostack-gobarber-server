b24adf634788717eeae092a04b0119fa0e909aa7
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-09-03 10:03:12 -0300

    added sentry

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -12,19 +12,31 @@ import ratelimiter from './middlewares/rateLimiter';
 import '@shared/infra/typeorm';
 import '@shared/container';
 
+import * as Sentry from '@sentry/node';
+
 const app = express();
+
+Sentry.init({
+  dsn:
+    'https://f469e21828d44d29a5cc3e2979f450c2@o442611.ingest.sentry.io/5416497',
+});
+app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
 
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(ratelimiter);
 app.use(routes);
-
 app.use(errors());
+
+app.get('/debug-sentry', function mainHandler(req, res) {
+  throw new Error('My first Sentry error!');
+});
 
 // Global errors interceptor
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
+    Sentry.captureException(err.message);
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
@@ -36,6 +48,8 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     message: 'Internal server error',
   });
 });
+
+app.use(Sentry.Handlers.errorHandler() as express.ErrorRequestHandler);
 
 app.listen(3333, () => {
   console.log('🚀 App running on port 3333');
